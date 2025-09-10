4048df05f28dbd472c1dd74ce405d08ff79c336e
Dániel Bordás <bordas.daniel0124@gmail.com>
2025-09-10 09:30:06 +0200

    Controllerre átszervezve az egész

---
+++ Controllers/EloadoController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zeneszamok.Models;
+
+namespace Zeneszamok.Controllers
+{
+    public class EloadoController
+    {
+        static MySqlConnection SqlConnection = new();
+
+        private static void BuildConnection()
+        {
+            try
+            {
+                var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+                string connectionString = config.GetConnectionString("Default");
+
+                SqlConnection.ConnectionString = connectionString;
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"Hiba történt az adatbázishoz való csatlakozás során: {ex}");
+            }
+            
+        }
+
+        public int Modosit(int szam, string nev, string nemzetiseg, bool szolo)
+        {
+            int sorokSzama = 0;
+            try
+            {
+                SqlConnection.Open();
+                string sql = "UPDATE `eloado` SET `Nev`= @nev,`Nemzetiseg`= @nemzetiseg,`Szolo`= @szolo WHERE Id = @id";
+                MySqlCommand cmd = new(sql, SqlConnection);
+                cmd.Parameters.AddWithValue("@nev", nev);
+                cmd.Parameters.AddWithValue("@nemzetiseg", nemzetiseg);
+                cmd.Parameters.AddWithValue("@szolo", szolo);
+                cmd.Parameters.AddWithValue("@id", szam);
+                sorokSzama = cmd.ExecuteNonQuery();
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Hiba történt a módosítás során: {0}", ex);
+            }
+            finally
+            {
+                SqlConnection.Close();
+            }
+            return sorokSzama;
+           
+        }
+
+        public int Torles(int szam)
+        {
+            int sorokSzama = 0;
+            
+            try
+            {
+                SqlConnection.Open();
+                string sql = "DELETE FROM `eloado` WHERE Id = @id";
+                MySqlCommand cmd = new(sql, SqlConnection);
+                cmd.Parameters.AddWithValue("@id", szam);
+                sorokSzama = cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba történt a törlés során {0}", ex);
+            }
+            finally 
+            {
+                SqlConnection.Close(); 
+            }
+          
+            return sorokSzama;
+        }
+
+        public int Felvesz(Eloado rogzitendo)
+        {
+            int sorokSzama = 0;
+            try
+            {
+                SqlConnection.Open();
+                string sql = "INSERT INTO `eloado`(`Nev`, `Nemzetiseg`, `Szolo`) VALUES (@nev,@nemzetiseg, @szolo)";
+                MySqlCommand cmd = new(sql, SqlConnection);
+                cmd.Parameters.AddWithValue("@nev", rogzitendo.Nev);
+                cmd.Parameters.AddWithValue("@nemzetiseg", rogzitendo.Nemzetiseg);
+                cmd.Parameters.AddWithValue("@szolo", rogzitendo.Szolo);
+                sorokSzama = cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba történt a felvétel során: {0}", ex);
+            }
+            finally
+            {
+                SqlConnection.Close();
+            }
+            
+            return sorokSzama;
+        }
+
+        public List<Eloado> Feltolt()
+        {
+            BuildConnection();
+            List<Eloado> eloadokLocal = [];
+            try
+            {
+                SqlConnection.Open();
+                string sql = "SELECT * FROM eloado";
+                MySqlCommand cmd = new(sql, SqlConnection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Eloado eloado = new();
+                    eloado.Id = reader.GetInt32("Id");
+                    eloado.Nev = reader.GetString("Nev");
+
+                    if (!reader.IsDBNull(2))
+                    {
+                        eloado.Nemzetiseg = reader.GetString("Nemzetiseg");
+                    }
+                    else
+                    {
+                        eloado.Nemzetiseg = null;
+                    }
+
+                    eloado.Szolo = reader.GetBoolean("Szolo");
+
+                    eloadokLocal.Add(eloado);
+
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Adatbázis hiba: {ex}");
+            }
+
+            SqlConnection.Close();
+            return eloadokLocal;
+        }
+
+
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using Zeneszamok.Models;
+using Zeneszamok.Controllers;
 
 namespace Zeneszamok
 {
     class Program
     {
-        static MySqlConnection SqlConnection = new();
+        
         static List<Eloado> eloadok;
        
 
@@ -18,7 +19,7 @@ namespace Zeneszamok
                 Console.WriteLine("Az előadók listája:\n");
                 for (int i = 0; i < eloadok.Count; i++)
                 {
-                    Console.WriteLine($"{i + 1}. {eloadok[i].ToString()}");
+                    Console.WriteLine($"{i + 1}. {eloadok[i]}");
                 }
                 Console.Write("Írd be az előadó számát amelyet törölni szeretnél (0 az vissza): ");
             }
@@ -60,16 +61,7 @@ namespace Zeneszamok
                 Console.Write("Szolo? (I/N)");
                 bool szolo = Console.ReadLine().ToUpper() == "I" ? true : false;
 
-
-                SqlConnection.Open();
-                string sql = "UPDATE `eloado` SET `Nev`= @nev,`Nemzetiseg`= @nemzetiseg,`Szolo`= @szolo WHERE Id = @id";
-                MySqlCommand cmd = new(sql, SqlConnection);
-                cmd.Parameters.AddWithValue("@nev", nev);
-                cmd.Parameters.AddWithValue("@nemzetiseg", nemzetiseg);
-                cmd.Parameters.AddWithValue("@szolo", szolo);
-                cmd.Parameters.AddWithValue("@id", szam);
-                sorokSzama = cmd.ExecuteNonQuery();
-
+                sorokSzama = new EloadoController().Modosit(szam, nev, nemzetiseg, szolo);
 
             }
             catch (MySqlException ex)
@@ -78,14 +70,15 @@ namespace Zeneszamok
             }
             finally
             {
-                Console.WriteLine(sorokSzama > 0 ? "Sikeres Módosítás!" : "Sikertelen művelet!");
-                SqlConnection.Close();
+                Console.WriteLine(sorokSzama > 0 ? "Sikeres Módosítás!" : "Sikertelen művelet!");     
                 Console.WriteLine("\n(Nyomj egy gombot a folytatáshoz.)");
                 Console.ReadKey();
                 Console.Clear();
                 Valaszto();
             }
         }
+
+       
 
         static void Torol()
         {
@@ -129,21 +122,16 @@ namespace Zeneszamok
             int sorokSzama = 0;
             try
             {
-                SqlConnection.Open();
-                string sql = "DELETE FROM `eloado` WHERE Id = @id";
-                MySqlCommand cmd = new(sql, SqlConnection);
-                cmd.Parameters.AddWithValue("@id", szam);
-                sorokSzama = cmd.ExecuteNonQuery();
-                
+                sorokSzama = new EloadoController().Torles(szam);
 
-            } catch (MySqlException ex)
+            }
+            catch (MySqlException ex)
             {
                 Console.WriteLine($"Adatbázis hiba: {ex}");
             }
             finally
             {
                 Console.WriteLine(sorokSzama > 0 ? "Sikeres törlés!" : "Sikertelen művelet!");
-                SqlConnection.Close();
                 Console.WriteLine("\n(Nyomj egy gombot a folytatáshoz.)");
                 Console.ReadKey();
                 Console.Clear();
@@ -151,6 +139,8 @@ namespace Zeneszamok
             }
 
         }
+
+       
 
         static void Kiir()
         {
@@ -168,35 +158,18 @@ namespace Zeneszamok
 
         static void Felvesz()
         {
-            static Eloado AdatFelvetel()
-            {
-                Console.Write("Add meg az előadó nevét: ");
-                string nev = Console.ReadLine();
-                Console.Write("Add meg az elődó nemzetiségét: ");
-                string nemzetiseg = Console.ReadLine();
-                Console.Write("Szolo? (I/N)");
-                bool szolo = Console.ReadLine().ToUpper() == "I" ? true : false;
-                return new Eloado(nev, nemzetiseg, szolo);
-            }
 
             int sorokSzama = 0;
             try
             {
-                Eloado rogzitendo = AdatFelvetel();
-                SqlConnection.Open();
-                string sql = "INSERT INTO `eloado`(`Nev`, `Nemzetiseg`, `Szolo`) VALUES (@nev,@nemzetiseg, @szolo)";
-                MySqlCommand cmd = new(sql, SqlConnection);
-                cmd.Parameters.AddWithValue("@nev", rogzitendo.Nev);
-                cmd.Parameters.AddWithValue("@nemzetiseg", rogzitendo.Nemzetiseg);
-                cmd.Parameters.AddWithValue("@szolo", rogzitendo.Szolo);
-                sorokSzama = cmd.ExecuteNonQuery();
-            } catch (MySqlException ex)
+                sorokSzama = new EloadoController().Felvesz(AdatFelvetel());
+            }
+            catch (MySqlException ex)
             {
                 Console.WriteLine($"Adatbázis hiba: {ex}");
             }
             finally
             {
-                SqlConnection.Close();
                 string uzenet = sorokSzama > 0 ? "Sikeres felvétel!" : "Sikertelen felvétel!";
                 Console.WriteLine(uzenet);
                 Console.WriteLine("\n(Nyomj egy gombot a folytatáshoz.)");
@@ -204,7 +177,18 @@ namespace Zeneszamok
                 Valaszto();
             }
 
+           
+        }
 
+        private static Eloado AdatFelvetel()
+        {
+            Console.Write("Add meg az előadó nevét: ");
+            string nev = Console.ReadLine();
+            Console.Write("Add meg az elődó nemzetiségét: ");
+            string nemzetiseg = Console.ReadLine();
+            Console.Write("Szolo? (I/N)");
+            bool szolo = Console.ReadLine().ToUpper() == "I" ? true : false;
+            return new Eloado(nev, nemzetiseg, szolo);
         }
 
         static void Valaszto()
@@ -271,65 +255,10 @@ namespace Zeneszamok
 
         }
 
-        private static void BuildConnection()
-        {
-            var config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", false, true)
-                .Build();
-
-            string connectionString = config.GetConnectionString("Default");
-
-            SqlConnection.ConnectionString = connectionString;
-        }
-
-        private static List<Eloado> EloadokFeltolt()
-        {
-            List<Eloado> eloadokLocal = [];
-
-            try
-            {
-                SqlConnection.Open();
-                string sql = "SELECT * FROM eloado";
-                MySqlCommand cmd = new(sql, SqlConnection);
-                MySqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    Eloado eloado = new();
-                    eloado.Id = reader.GetInt32("Id");
-                    eloado.Nev = reader.GetString("Nev");
-
-                    if (!reader.IsDBNull(2))
-                    {
-                        eloado.Nemzetiseg = reader.GetString("Nemzetiseg");
-                    }
-                    else
-                    {
-                        eloado.Nemzetiseg = null;
-                    }
-
-                    eloado.Szolo = reader.GetBoolean("Szolo");
-
-                    eloadokLocal.Add(eloado);
-
-                }
-            } catch (MySqlException ex)
-            {
-                Console.WriteLine($"Adatbázis hiba: {ex}");
-            }
-            finally
-            {
-                SqlConnection.Close();
-            }
-
-
-            return eloadokLocal;
-        }
-
-
+       
         static void Main(string[] args)
         {
-            BuildConnection();
-            eloadok = EloadokFeltolt();
+            eloadok = new EloadoController().Feltolt();
             Valaszto();
 
 
